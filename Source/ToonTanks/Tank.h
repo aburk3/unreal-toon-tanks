1ddbfbe25e8abfbf0a69e390c162e9f469cddda7
Austin Burke <austinlayneburke@gmail.com>
2021-10-12 06:53:41 -0500

    Handle actor local rotation w/ turnrate

--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -29,5 +29,9 @@ class TOONTANKS_API ATank : public ABasePawn
 		UPROPERTY(EditAnywhere, Category = "Movement")
 		float Speed = 200.f;
 
+		UPROPERTY(EditAnywhere, Category = "Movement")
+		float TurnRate = 45.f;
+
 		void Move(float Value);
+		void Turn(float Value);
 };
