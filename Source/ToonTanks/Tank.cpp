8649b1bdafa2c0f0fe24bb8dcff2578c864afbfb
Austin Burke <austinlayneburke@gmail.com>
2021-10-13 05:59:53 -0500

    Added debug collision sphere

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -5,6 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 ATank::ATank()
 {
@@ -24,11 +25,37 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
     PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
 }
 
+void ATank::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (PlayerControllerRef)
+    {
+        FHitResult HitResult;
+        PlayerControllerRef->GetHitResultUnderCursor(
+            ECollisionChannel::ECC_Visibility,
+            false,
+            HitResult
+        );
+        
+        DrawDebugSphere(
+            GetWorld(), 
+            HitResult.ImpactPoint,
+            25.f,
+            12,
+            FColor::Red,
+            false,
+            -1.f
+        );
+    }
+}
+
 void ATank::BeginPlay()
 {
     Super::BeginPlay();
 
     PlayerControllerRef = Cast<APlayerController>(GetController());
+
 }
 
 void ATank::Move(float Value)
